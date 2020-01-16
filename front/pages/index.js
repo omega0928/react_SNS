7b9cae4c32ae389cc94fb215ed370ffc4ad591f6
gs.jung <omega0928@naver.com>
2020-01-17 00:01:42 +0900

    Layout initial

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import Link from 'next/link'
+import Link from 'next/link';
+import Head from 'next/head';
+import AppLayout from '../components/AppLayout';
 
 const Home = () => {
     return (
         <>
-            <Link href="/about"><a>about</a></Link>
-            <div>Hello</div>
+            <Head>
+                <title>NodeBird</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.25.3/antd.css" />
+            </Head>
+            <AppLayout>
+                <Link href="/about"><a>about</a></Link>
+                <div>Hello</div>
+            </AppLayout>
         </>
     );
 };
